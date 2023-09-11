4367e29f3d4a73910adc53612801b532a9574e9c
Ksu977 <brosup490@gmail.com>
2023-09-11 17:46:05 -0400

    Machine class and motor initialization

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Machine.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.Util;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+public class Machine {
+
+
+        // DT motors
+        public DcMotor frontRight;
+
+        public DcMotor frontLeft;
+        public DcMotor backRight;
+        public DcMotor backLeft;
+        HardwareMap hardwareMap;
+
+        public Machine(HardwareMap hwMap){
+            initialize(hwMap);
+        }
+        private void initialize(HardwareMap hwMap) {
+            hardwareMap = hwMap;
+
+            //Connect Motors
+            frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+            frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+            backRight = hardwareMap.get(DcMotor.class, "backRight");
+            backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+
+            //Set motor direction
+            frontRight.setDirection(DcMotor.Direction.FORWARD);
+            frontLeft.setDirection(DcMotor.Direction.REVERSE);
+            backRight.setDirection(DcMotor.Direction.FORWARD);
+            backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+            // Drivetrain motors
+            backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        }
+}
