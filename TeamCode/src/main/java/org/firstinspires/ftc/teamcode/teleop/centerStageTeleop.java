6ba470da96ffab4fc3cb9f86f10b848a8784fed4
spo0n9 <aarush.araj2008@gmail.com>
2023-12-02 19:05:49 -0500

    Drive Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/centerStageTeleop.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.util.Machine;
+
+@TeleOp
+public class centerStageTeleop extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Machine robot = new Machine(hardwareMap);
+        double mode = 1;
+        waitForStart();
+
+        ElapsedTime timer = new ElapsedTime();
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            robot.frontLeft.setPower(frontLeftPower / mode);
+            robot.backLeft.setPower(backLeftPower / mode);
+            robot.frontRight.setPower(frontRightPower / mode);
+            robot.backRight.setPower(backRightPower / mode);
+        }
+    }
+}
